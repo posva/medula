4135272b6fa1f3c08298060f2900fb2c11349370
Eduardo San Martin Morote <posva13@gmail.com>
2026-09-15 11:24:54 +0200

    style: lint

--- playgrounds/svelte-vite/src/Item.svelte
+++ playgrounds/svelte-vite/src/Item.svelte
@@ -4,6 +4,11 @@
   let highlighted = $state(false)
 </script>
 
-<li class:done class:highlighted onpointerenter={() => (highlighted = true)} onpointerleave={() => (highlighted = false)}>
+<li
+  class:done
+  class:highlighted
+  onpointerenter={() => (highlighted = true)}
+  onpointerleave={() => (highlighted = false)}
+>
   <label><input type="checkbox" bind:checked={done} /> {label}</label>
 </li>
